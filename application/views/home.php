a439080e25976fbde77d7ff7bbe288fa68647269
Lsagardia <sagardia.linn.787@gmail.com>
2024-10-20 23:55:44 -0400

    cambios en la vista home cuando ingresa el usuario bibiliotecario

--- application/views/home.php
+++ application/views/home.php
@@ -1,5 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Importa la fuente desde Google Fonts -->
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Spicy+Rice&display=swap');
+
+        /* Define la clase para la fuente */
+        .spicy-rice-regular {
+            font-family: "Spicy Rice", serif;
+            font-weight: 300;
+            font-style: normal;
+        }
+    </style>
+</head>
+<body>
 <div class="container">
-    <h1>Bienvenido a eLibroCocha</h1>
+    <h1 class="spicy-rice-regular">Bienvenido a eLibroCocha</h1>
     
     <div class="row">
         <div class="col-md-6">
@@ -17,3 +33,5 @@
         </div>
     </div>
 </div>
+</body>
+</html>
